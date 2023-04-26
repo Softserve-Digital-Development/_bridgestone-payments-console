e0c270c49d5c489e57a3b8ab18ff07c3f27a48a9
Eli-Softserve <eli@softservedigital.co.za>
2023-04-26 09:52:32 +0200

    Removed unused code

--- src/RunBatchPayments/Program.cs
+++ src/RunBatchPayments/Program.cs
@@ -2,10 +2,4 @@
 using HttpClient client = new();
 string url = "https://localhost:7025/BatchPayment/RunBatch/?includeDateFilters=True";
 var request = new HttpRequestMessage(HttpMethod.Post, url);
-await ProcessAsync(client, request);
-
-static async Task ProcessAsync(HttpClient client, HttpRequestMessage request)
-{
-    var json = await client.SendAsync(request);
-    Console.WriteLine(json);
-}
+await client.SendAsync(request);
