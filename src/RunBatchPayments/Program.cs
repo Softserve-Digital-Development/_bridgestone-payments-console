de258a0e889d1dd4c68075ef348a0ca68ebfe6b7
Eli-Softserve <eli@softservedigital.co.za>
2023-04-26 09:26:46 +0200

    Sending out the request

--- src/RunBatchPayments/Program.cs
+++ src/RunBatchPayments/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using HttpClient client = new();
+string url = "https://localhost:7025/BatchPayment/RunBatch/?includeDateFilters=True";
+var request = new HttpRequestMessage(HttpMethod.Post, url);
+await ProcessAsync(client, request);
 
-Console.WriteLine("Hello, World!");
+static async Task ProcessAsync(HttpClient client, HttpRequestMessage request)
+{
+    var json = await client.SendAsync(request);
+    Console.WriteLine(json);
+}
